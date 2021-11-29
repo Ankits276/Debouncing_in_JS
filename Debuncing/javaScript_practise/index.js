a871ea8802675dba73a0ca5d608c010b994aa4da
Ankits276 <satoniyaankit@gmail.com>
2021-11-29 16:56:08 +0530

    Debouncing in JS

---
+++ Debuncing/javaScript_practise/index.js
@@ -0,0 +1,20 @@
+// let arr = [1, 2, 3, 4];
+
+// let arr2 = arr;
+// arr.length = 0;
+
+// console.log(arr2, arr);
+
+// ===================
+
+// isInt(12.2) //false
+// isInt(4) //true
+// isInt(0.4) //false
+// =======================
+
+isInt = (num) => num === (num | 0);
+
+// || => Logical OR
+// | => bitwise OR
+// && => logical And
+// & => bitwise and
